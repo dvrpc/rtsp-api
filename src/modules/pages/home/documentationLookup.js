6036662e54ab3628ebdab9578da2ffac9bd7af21
Kris Warner <kdwarn@protonmail.com>
2021-08-23 14:09:06 -0400

    Remove front-end code

--- src/modules/pages/home/documentationLookup.js
+++
@@ -1,8 +0,0 @@
-const documentationLookup = {
-    'Transit Network Gap Analyzer': 'TransitNetworkGapAnalyzer.pdf',
-    'Surface Transit Reliability': 'SurfaceTransitReliability.pdf',
-    'Higher Frequency Scenario': 'HigherFrequencyScenario.pdf',
-    'Wheelchair Accessibility': 'WheelchairAcessibility.pdf'
-}
-
-export default documentationLookup
