f1c8201e8e3017b85eb28b5b6841c2f7221e6ef9
Kris Warner <kdwarn@protonmail.com>
2023-01-04 15:22:51 -0500

    Rm municipalities endpoint from gap.py
    (Already moved to municipalities.py.)

--- api/gap.py
+++ api/gap.py
@@ -11,25 +11,6 @@ from db import db
 router = fastapi.APIRouter()
 
 
-# NOTE: had been "/api/rtps/v1/gap?list"
-@router.get("/api/rtps/v1/municipalities")
-def mun_list():
-    with db(PG_CREDS) as cursor:
-        cursor.execute(
-            """
-            SELECT mun_name, geoid
-            FROM zonemcd_join_region_wpnr_trim
-            GROUP BY mun_name, geoid
-            ORDER BY mun_name
-            """
-        )
-        results = cursor.fetchall()
-    list = []
-    for row in results:
-        list.append([row[0], row[1]])
-    return list
-
-
 # TODO: sort this one out
 # NOTE: had been "/api/rtps/gap?zones=[xxx]&direction=xx
 @router.get("/api/rtps/v1/gap/zones")
